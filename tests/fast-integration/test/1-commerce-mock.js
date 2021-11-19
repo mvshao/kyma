12544c6c39849d71496930d15d99b33a41d6ec30
Rafał Foks <r.fok74@gmail.com>
2021-11-19 14:14:48 +0100

    Added test for OSS

--- tests/fast-integration/test/1-commerce-mock.js
+++ tests/fast-integration/test/1-commerce-mock.js
@@ -8,6 +8,8 @@ const {
   ensureCommerceMockLocalTestFixture,
   checkFunctionResponse,
   sendEventAndCheckResponse,
+  renewCommerceMockCertificate,
+  revokeCommerceMockCertificate,
   cleanMockTestFixture,
   checkInClusterEventDelivery,
 } = require("./fixtures/commerce-mock");
@@ -15,11 +17,9 @@ const {
   printRestartReport,
   getContainerRestartsForAllNamespaces,
 } = require("../utils");
-const {
-  checkLokiLogs,
-  lokiPortForward
-} = require("../logging");
+const { checkLokiLogs, lokiPortForward } = require("../logging");
 
+<<<<<<< HEAD
 function commerceMockTests() {
   describe("CommerceMock tests", function () {
     this.timeout(10 * 60 * 1000);
@@ -29,6 +29,17 @@ function commerceMockTests() {
     const testStartTimestamp = new Date().toISOString();
     let initialRestarts = null;
     let cancelPortForward = null;
+=======
+describe("CommerceMock tests", function () {
+  this.timeout(10 * 60 * 1000);
+  this.slow(5000);
+  const withCentralAppConnectivity =
+    process.env.WITH_CENTRAL_APP_CONNECTIVITY === "true";
+  const testNamespace = "test";
+  const testStartTimestamp = new Date().toISOString();
+  let initialRestarts = null;
+  let cancelPortForward = null;
+>>>>>>> 1a3bda247 (Added test for OSS)
 
     before(() => {
       cancelPortForward = lokiPortForward();
@@ -42,11 +53,26 @@ function commerceMockTests() {
       initialRestarts = await getContainerRestartsForAllNamespaces();
     });
 
+<<<<<<< HEAD
     it("CommerceMock test fixture should be ready", async function () {
       await ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity).catch((err) => {
         console.dir(err); // first error is logged
         return ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity);
       });
+=======
+  it("CommerceMock test fixture should be ready", async function () {
+    await ensureCommerceMockLocalTestFixture(
+      "mocks",
+      testNamespace,
+      withCentralAppConnectivity
+    ).catch((err) => {
+      console.dir(err); // first error is logged
+      return ensureCommerceMockLocalTestFixture(
+        "mocks",
+        testNamespace,
+        withCentralAppConnectivity
+      );
+>>>>>>> 1a3bda247 (Added test for OSS)
     });
 
     it("in-cluster event should be delivered (structured and binary mode)", async function () {
@@ -61,6 +87,7 @@ function commerceMockTests() {
       await sendEventAndCheckResponse();
     });
 
+<<<<<<< HEAD
     it("Should print report of restarted containers, skipped if no crashes happened", async function () {
       const afterTestRestarts = await getContainerRestartsForAllNamespaces();
       printRestartReport(initialRestarts, afterTestRestarts);
@@ -69,6 +96,36 @@ function commerceMockTests() {
     it("Logs from commerce mock pod should be retrieved through Loki", async function() {
       await checkLokiLogs(testStartTimestamp);
     });
+=======
+  // renew certificate
+  it("CommerceMock should renew it's certificate", async function () {
+    await renewCommerceMockCertificate();
+  });
+  // call lambda and succeed
+  it("order.created.v1 event should trigger the lastorder function", async function () {
+    await sendEventAndCheckResponse();
+  });
+
+  // revoke certificate
+  it("should revoke Commerce Mock certificate", async function () {
+    await revokeCommerceMockCertificate();
+  });
+
+  /* TODO why is this passing, after revoking the cert
+  // call lambda and FAIL
+  it("order.created.v1 event should trigger the lastorder function", async function () {
+    await sendEventAndCheckResponse();
+  });*/
+
+  it("Should print report of restarted containers, skipped if no crashes happened", async function () {
+    const afterTestRestarts = await getContainerRestartsForAllNamespaces();
+    printRestartReport(initialRestarts, afterTestRestarts);
+  });
+
+  it("Logs from commerce mock pod should be retrieved through Loki", async function () {
+    await checkLokiLogs(testStartTimestamp);
+  });
+>>>>>>> 1a3bda247 (Added test for OSS)
 
     it("Test namespaces should be deleted", async function () {
       await cleanMockTestFixture("mocks", testNamespace, true);
