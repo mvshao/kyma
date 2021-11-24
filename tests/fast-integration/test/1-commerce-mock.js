0f7136a3f99aa5e95a6a56cf46b57a7a4d628536
Rafał Foks <r.fok74@gmail.com>
2021-11-24 11:51:51 +0100

    Resolve conflicts

--- tests/fast-integration/test/1-commerce-mock.js
+++ tests/fast-integration/test/1-commerce-mock.js
@@ -19,7 +19,6 @@ const {
 } = require("../utils");
 const { checkLokiLogs, lokiPortForward } = require("../logging");
 
-<<<<<<< HEAD
 function commerceMockTests() {
   describe("CommerceMock tests", function () {
     this.timeout(10 * 60 * 1000);
@@ -29,17 +28,6 @@ function commerceMockTests() {
     const testStartTimestamp = new Date().toISOString();
     let initialRestarts = null;
     let cancelPortForward = null;
-=======
-describe("CommerceMock tests", function () {
-  this.timeout(10 * 60 * 1000);
-  this.slow(5000);
-  const withCentralAppConnectivity =
-    process.env.WITH_CENTRAL_APP_CONNECTIVITY === "true";
-  const testNamespace = "test";
-  const testStartTimestamp = new Date().toISOString();
-  let initialRestarts = null;
-  let cancelPortForward = null;
->>>>>>> 1a3bda247 (Added test for OSS)
 
     before(() => {
       cancelPortForward = lokiPortForward();
@@ -53,26 +41,11 @@ describe("CommerceMock tests", function () {
       initialRestarts = await getContainerRestartsForAllNamespaces();
     });
 
-<<<<<<< HEAD
     it("CommerceMock test fixture should be ready", async function () {
       await ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity).catch((err) => {
         console.dir(err); // first error is logged
         return ensureCommerceMockLocalTestFixture("mocks", testNamespace, withCentralAppConnectivity);
       });
-=======
-  it("CommerceMock test fixture should be ready", async function () {
-    await ensureCommerceMockLocalTestFixture(
-      "mocks",
-      testNamespace,
-      withCentralAppConnectivity
-    ).catch((err) => {
-      console.dir(err); // first error is logged
-      return ensureCommerceMockLocalTestFixture(
-        "mocks",
-        testNamespace,
-        withCentralAppConnectivity
-      );
->>>>>>> 1a3bda247 (Added test for OSS)
     });
 
     it("in-cluster event should be delivered (structured and binary mode)", async function () {
@@ -87,16 +60,6 @@ describe("CommerceMock tests", function () {
       await sendEventAndCheckResponse();
     });
 
-<<<<<<< HEAD
-    it("Should print report of restarted containers, skipped if no crashes happened", async function () {
-      const afterTestRestarts = await getContainerRestartsForAllNamespaces();
-      printRestartReport(initialRestarts, afterTestRestarts);
-    });
-
-    it("Logs from commerce mock pod should be retrieved through Loki", async function() {
-      await checkLokiLogs(testStartTimestamp);
-    });
-=======
   // renew certificate
   it("CommerceMock should renew it's certificate", async function () {
     await renewCommerceMockCertificate();
@@ -125,7 +88,6 @@ describe("CommerceMock tests", function () {
   it("Logs from commerce mock pod should be retrieved through Loki", async function () {
     await checkLokiLogs(testStartTimestamp);
   });
->>>>>>> 1a3bda247 (Added test for OSS)
 
     it("Test namespaces should be deleted", async function () {
       await cleanMockTestFixture("mocks", testNamespace, true);
